2faac08e77f3ff2318060c19caa82c64dcac8b65
danagajewski <danagajewski2018@gmail.com>
2022-04-21 17:42:27 -0400

    Got spotify api working

--- controllers/spotify-controller-new.js
+++ controllers/spotify-controller-new.js
@@ -1,29 +1,44 @@
-// const SpotifyWebApi = require('spotify-web-api-node');
-import SpotifyWebApi from 'spotify-web-api-node';
-const spotifyControllerNew = (app) => {
-  app.post('/api/login', login);
-  app.post('/api/refresh', refresh);
-}
 
-const login = (req, res) => {
-  const code = req.body.code
-  const spotifyApi = new SpotifyWebApi({
-    redirectUri: process.env.REDIRECT_URI,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-  })
-  spotifyApi
-  .authorizationCodeGrant(code)
-  .then(data => {
-    res.json({
-      accessToken: data.body.access_token,
-      refreshToken: data.body.refresh_token,
-      expiresIn: data.body.expires_in,
+import SpotifyWebApi from 'spotify-web-api-node';
+import request from "request";
+import fetch from 'node-fetch';
+import axios from "axios";
+import * as querystring from "querystring";
+
+const spotifyControllerNew = (app) => {
+  //app.post('/api/login', login);
+  app.post('/api/refresh', refresh);
+  app.get('/api/getToken/:code', function(req, res){
+    const thiscode = req.params.code;
+    const client_id =  "120d6eb199c6499998e0fe645309337b";
+    const client_secret= "36901dcde12a499ca52941814552ca78";
+    const headers = {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      }
+    };
+
+    let data = {
+      grant_type: "client_credentials",
+      code: thiscode,
+      redirectUri: "http://localhost:8000/callback",
+      client_id: client_id,
+      client_secret: client_secret,
+    };
+    axios
+    .post(
+        "https://accounts.spotify.com/api/token",
+        querystring.stringify(data),
+        headers
+    )
+    .then((response) => {
+      console.log(response.data);
+      res.json(response.data);
     })
-  })
-  .catch(err => {
-    res.sendStatus(400)
-  })
+    .catch((error) => {
+      console.log(error);
+    });
+  });
 }
 
 const refresh = (req, res) => {
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import userController from "./controllers/user-controller.js";
 import notesController from "./controllers/notes-controller.js";
 import spotifyControllerNew from "./controllers/spotify-controller-new.js";
 import mongoose from "mongoose";
-const CONNECTION_STRING = "mongodb+srv://dgajewski:dontstealmydata@mixtape.f3ojk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"//process.env.MIXTAPE_DB_CONNECTION_STRING
+const CONNECTION_STRING = "mongodb+srv://dgajewski:dontstealmydata@mixtape.f3ojk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";//process.env.MIXTAPE_DB_CONNECTION_STRING;
 mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors({credentials: true,
